aef9391c9aac23c0701c78a23bef95cd8aae5a31
LlechiKaito <124239629+LlechiKaito@users.noreply.github.com>
2024-05-18 06:53:51 +0900

    prismaの修正

--- backend/prisma/migrations/20240502114739_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `user_image` MODIFY `file_name` VARCHAR(191) NULL,
-    MODIFY `another_file_name` VARCHAR(191) NULL;
--- backend/prisma/migrations/20240511102756_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `password_reset_expires` DATETIME(3) NULL,
-    ADD COLUMN `password_reset_token` VARCHAR(191) NULL;
